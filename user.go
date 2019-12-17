3f6feac9f52ee75ad9034388023e47f7e8a3c249
Maximilian Mitchell <max@maxis.me>
2019-12-17 22:52:10 +0000

    remove logging

--- user.go
+++ user.go
@@ -186,10 +186,6 @@ func IsValidUserCredentials(db *sql.DB, user User) bool {
         FROM user
         WHERE UUID = ? AND UUID_key = ?`, Hash(user.UUID), Hash(user.UUIDKey))
 		err := result.Scan(&id)
-		Handle(err)
-		log.Println(id)
-		log.Println(user.UUID)
-		log.Println(user.UUIDKey)
 		if err == nil && id > 0 {
 			return true
 		}
