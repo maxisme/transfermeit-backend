5f76b60b8e946da9a6536af792879fbf5850961c
Maximilian Mitchell <max@maxis.me>
2019-12-17 22:46:18 +0000

    add logging to identify why test is failing

--- user.go
+++ user.go
@@ -186,6 +186,8 @@ func IsValidUserCredentials(db *sql.DB, user User) bool {
         FROM user
         WHERE UUID = ? AND UUID_key = ?`, Hash(user.UUID), Hash(user.UUIDKey))
 		err := result.Scan(&id)
+		Handle(err)
+		log.Println(id)
 		if err == nil && id > 0 {
 			return true
 		}
