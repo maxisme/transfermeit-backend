b555717e6574137053823a45d1925bd3b61a0ba4
Maximilian Mitchell <max@maxis.me>
2020-01-04 13:30:53 +0000

    fix deploy script

--- deploy.sh
+++ deploy.sh
@@ -4,7 +4,7 @@
 # jenk ALL = NOPASSWD: /bin/bash /root/transfermeit-backend/deploy.sh
 # remember to add .env file
 
-cd /root/transfermeit-backend/
+cd $(dirname "$0")
 
 git fetch &> /dev/null
 diffs=$(git diff master origin/master)
@@ -19,7 +19,8 @@ then
     docker-compose up flyway
 
     # update server
-    docker-compose up --build -d
+    docker-compose build app
+    docker-compose up --no-deps -d app
 
     # kill all unused dockers
     docker system prune -f
