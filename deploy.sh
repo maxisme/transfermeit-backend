6befae65ce8d874ca93b9c7f7cb5e86dd3ecc307
Maximilian Mitchell <max@max.me.uk>
2020-08-13 19:00:25 +0100

    update stack and deploy script

--- deploy.sh
+++ deploy.sh
@@ -2,3 +2,7 @@
 git pull
 export $(grep -v '^#' .env | xargs)
 docker stack deploy -c stack.yml transfmeit-backend
+
+export TAG="develop"
+export SUBDOM="$TAG."
+docker stack deploy -c stack.yml dev-transfmeit-backend
